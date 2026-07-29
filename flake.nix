0d447d50a04667b6eef270dbaad33c999438a4c9
yinbailiang <yinbailiang@foxmail.com>
2026-07-29 12:49:53 +0800

    支持nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Powershell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.powershell
+            ];
+
+            shellHook = ''
+            exec pwsh
+            '';
+          };
+        }
+      );
+    };
+}
